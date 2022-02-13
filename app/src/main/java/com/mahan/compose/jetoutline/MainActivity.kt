7087af1d1b91b295796eb30962214dfdc87abc4d
Mahan Ziyari <maziyar.mz.021@gmail.com>
2022-02-13 20:06:24 +0330

    Add ViewModel and Navigation Dependencies

--- app/src/main/java/com/mahan/compose/jetoutline/MainActivity.kt
+++ app/src/main/java/com/mahan/compose/jetoutline/MainActivity.kt
@@ -16,28 +16,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetOutlineTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.surface
-                ) {
-                    Greeting("Android")
-                }
+            JetOutline {
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun JetOutline(content: @Composable () -> Unit) {
+    JetOutlineTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.surface
+        ) {
+            content()
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetOutlineTheme {
-        Greeting("Android")
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.surface
+        ) {}
     }
 }
